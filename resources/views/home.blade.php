723f65d1219050312ce68ace7cffd75df009e945
DrDankman <imdanielc@gmail.com>
2019-05-22 10:27:43 +0200

    form knappar ordnade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,6 +7,11 @@
         </a>
     </div>
     <div class="buttondiv">
+        <form action="/uploadfile" method="post">
+            <input type="file" name="" id="">
+            <input type="submit" value="Upload file">
+        
+        </form>
         <button class="button" type="button">Upload and filter</button>
     </div>
 @endsection
