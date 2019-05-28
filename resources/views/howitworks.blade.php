d5f73c6f5874c53c8bbbd192ddbd942ec9744c3e
DrDankman <imdanielc@gmail.com>
2019-05-28 23:12:52 +0200

    sample text inserted

--- resources/views/howitworks.blade.php
+++ resources/views/howitworks.blade.php
@@ -5,5 +5,8 @@
         <a class="howitworkstext" href="/">
             Back to home
         </a>
-    </div>    
+    </div>
+    <div class="howitworksdiv">
+            Steve Harvey: "We asked 100 people, what is the male reproductive organ?" Contestant: "The penis" SH: "A WUH... HUH??" audience erupts into laughter Steve Harvey grabs onto podium to support himself laughter gets even louder SH: O lordy... one man goes into cardiac arrest and many others begin vomiting profusely from laughing too hard SH: YOU PEOPLE NEED HELP the Earth shatters and Satan rises from the underworld to claim unworthy souls the universe begins rapidly closing in on itself SH: (putting on a weary voice) Survey says... the board shows 100 for "penis" Harvey is able to get off one more shocked look before existence as we know it comes to an end
+    </div>
 @endsection
